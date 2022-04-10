519dffa041d0b57e8eb5cfdb08708814e2ee29a9
ssmyth <ssmythw@outlook.com>
2022-04-10 11:31:56 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'password-generator';
+  length = 0;
+  password = '';
+  alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  numbers = '0123456789';
+  specialChars = '!@#$%^&*()_+';
+
+  contactForm = new FormGroup({
+    length: new FormControl(),
+    numbers: new FormControl(),
+    specialChars: new FormControl(),
+  });
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+    let str = this.alpha;
+    if (this.contactForm.value.numbers == true) {
+      str = str + this.numbers;
+    }
+    if (this.contactForm.value.specialChars == true) {
+      str = str + this.specialChars;
+    }
+    let pw = '';
+    for (let i = 0; i < this.contactForm.value.length; i++) {
+      // for each iteration choose a random index of str and append it onto pw
+      let randomNumber = Math.floor(Math.random() * str.length);
+      pw = pw + str[randomNumber];
+    }
+    console.log(pw);
+    this.password = pw;
+  }
+  onSliderChange() {
+    this.length = this.contactForm.value.length;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
